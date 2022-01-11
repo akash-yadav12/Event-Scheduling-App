cb7aa96a442d1851673d4cc95741056eb42899b6
akash-yadav12 <akash.ry@somaiya.edu>
2022-01-11 12:42:35 +0530

    unused code remove

--- src/components/UI/AuthForm.js
+++ src/components/UI/AuthForm.js
@@ -2,37 +2,33 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import ToastMessage from "../UI/ToastMessage";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = (props) => {
   return (
-    <>
-      {props.showToast ? <ToastMessage message={props.toastContent} /> : null}
-      <form onSubmit={props.submitHandler} className={classes.form}>
-        <h2>{props.title}</h2>
-        <div className={classes.input}>
-          <label htmlFor="email">Email:</label>
-          <input
-            onChange={props.emailChangeHandler}
-            id="email"
-            type="email"
-            placeholder="enter email"
-          />
-        </div>
-        <div className={classes.input}>
-          <label htmlFor="password">Password:</label>
-          <input
-            minLength="4"
-            onChange={props.passwordChangeHandler}
-            id="password"
-            type="password"
-            placeholder="enter password"
-          />
-        </div>
-        <button type="submit">{props.btnText}</button>
-      </form>
-    </>
+    <form onSubmit={props.submitHandler} className={classes.form}>
+      <h2>{props.title}</h2>
+      <div className={classes.input}>
+        <label htmlFor="email">Email:</label>
+        <input
+          onChange={props.emailChangeHandler}
+          id="email"
+          type="email"
+          placeholder="enter email"
+        />
+      </div>
+      <div className={classes.input}>
+        <label htmlFor="password">Password:</label>
+        <input
+          minLength="4"
+          onChange={props.passwordChangeHandler}
+          id="password"
+          type="password"
+          placeholder="enter password"
+        />
+      </div>
+      <button type="submit">{props.btnText}</button>
+    </form>
   );
 };
 
@@ -42,8 +38,6 @@ AuthForm.propTypes = {
   btnText: PropTypes.string,
   emailChangeHandler: PropTypes.func,
   passwordChangeHandler: PropTypes.func,
-  showToast: PropTypes.bool,
   submitHandler: PropTypes.func,
   title: PropTypes.string,
-  toastContent: PropTypes.string,
 };
